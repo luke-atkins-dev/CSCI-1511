cb52ea76d2ed3c152b684f2e18a44f019a14759b
aaron <aaron>
2026-01-20 12:58:25 -0500

    Fixed formatting

--- Unit 2/participation_activity.py
+++ Unit 2/participation_activity.py
@@ -1,13 +1,14 @@
 
 """
-Purpose: This program uses a user's static name and will print the lower, upper, and title case version centered with underscores
+Purpose: This program uses a user's static name and will print the lower, upper, and title case version centered with padding
 Author: Luke Atkins
 Starter Code: No starter code used
 Date: 1/20/2026
 """
 
 def main(name):
-    print_name = lambda func: print(f'{func(name):_^20}')
+    padding = " " * 5
+    print_name = lambda func: print(f'{padding}{func(name)}{padding}')
 
     print_name(str.lower)
     print_name(str.upper)
