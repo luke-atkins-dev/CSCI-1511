b22b9a694925d522e2f7f8b37de1232e3f152e27
aaron <aaron>
2026-01-20 13:23:12 -0500

    removed dirs

--- participation_activity.py
+++ participation_activity.py
@@ -1,14 +1,14 @@
 
 """
-Purpose: This program uses a user's static name and will print the lower, upper, and title case version centered with padding
+Purpose: This program uses a user's static name and will print the lower, upper, and title case version centered with spaces
 Author: Luke Atkins
 Starter Code: No starter code used
 Date: 1/20/2026
 """
 
 def main(name):
-    padding = " " * 5
-    print_name = lambda func: print(f'{padding}{func(name)}{padding}')
+    padding = 5
+    print_name = lambda func: print(f'{func(name):^{len(name)+(padding*2)}}')
 
     print_name(str.lower)
     print_name(str.upper)
